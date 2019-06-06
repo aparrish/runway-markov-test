8f2efae6b621891156fe95ffe33e76c42f1177c2
Allison Parrish <allison@decontextualize.com>
2019-06-06 17:28:04 -0400

    hey sure let's do this

---
+++ README.md
@@ -0,0 +1,6 @@
+# Allison tries to make a Runway model
+
+oh I get the pun now. runway model.
+
+anyway, this just does prediction on a pre-trained Markov model of some
+randomly selected novels from Project Gutenberg (using Markovify).
---
+++ runway_model.py
@@ -0,0 +1,24 @@
+import runway
+from runway.data_types import text, number
+
+import gzip
+
+from markovify import Text
+
+@runway.setup
+def setup():
+    msg = '[SETUP]'
+    with gzip.open('./novel-model-markovify.json.gz') as fh:
+        return Text.from_json(fh.read())
+
+@runway.command(name='generate',
+                inputs={'max_len': number(default=80)},
+                outputs={'output': text()})
+def generate(model, args):
+    print('[GENERATE] Ran with max_len value "{}"'.format(args['max_len']))
+    output = model.make_short_sentence(args['max_len'])
+    return {'output': output}
+
+if __name__ == '__main__':
+    runway.run(host='0.0.0.0', port=8000)
+
